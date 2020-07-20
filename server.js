6b29ddfcdb4b9e7b83a4fb61b3dee065ab6a1b49
your_name <your_email>
2020-07-20 17:56:36 +0800

    dfidfjid

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const db =
 knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'nick981837',
-    database : 'smart_brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 

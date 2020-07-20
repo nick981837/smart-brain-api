54da99f69d1de22866c82b72a044764721c9c12d
your_name <your_email>
2020-07-20 15:23:32 +0800

    changing server

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const app = express();
 const cors = require('cors');
-const  knex = require('knex');
+const knex = require('knex');
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-app.use(bodyParser.json())
-app.use(cors());
+
 
 const db =
 knex({
@@ -23,9 +23,11 @@ knex({
   }
 });
 
-db.select('*').from('users')
-.then(data=>console.log(data));
 
+const app =express();
+
+app.use(bodyParser.json())
+app.use(cors());
 
 
 app.get('/', (req, res)=>{res.send('it is working')})
